68efa8cf4f815bbab662be33f615e43acaf6187a
kst <kstost@gmail.com>
2022-03-11 14:40:41 +0900

    Create AudioAdvManager.js

---
+++ AudioAdvManager.js
@@ -0,0 +1,139 @@
+class AudioAdvManager {
+    static __silent = '//uwBAAP8AAAaQAAAAgAAA0gAAABAAABpAAAACAAADSAAAAETEFNRTMuOTkuNVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVUxBTUUzLjk5LjVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVX/+7IEAA/wAABpAAAACAAADSAAAAEAAAGkAAAAIAAANIAAAARMQU1FMy45OS41VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVUxBTUUzLjk5LjVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVX/+7IEAA/wAABpAAAACAAADSAAAAEAAAGkAAAAIAAANIAAAARMQU1FMy45OS41VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVUxBTUUzLjk5LjVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVX/+7IEAA/wAABpAAAACAAADSAAAAEAAAGkAAAAIAAANIAAAARMQU1FMy45OS41VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVUxBTUUzLjk5LjVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVX/+7IEAA/wAABpAAAACAAADSAAAAEAAAGkAAAAIAAANIAAAARMQU1FMy45OS41VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVUxBTUUzLjk5LjVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVU=';
+    interacted = false;
+    playingInstances = {};
+    playingDate = {};
+    gains = {};
+    constructor() {
+        this.audioContext = new AudioContext();
+    }
+    async loadData(audios) {
+        if (!audios) return;
+        audios = { ...audios };
+        if (this.absList === undefined) this.absList = {};
+        if (!this.absList['__silent']) audios['__silent'] = AudioAdvManager.__silent;
+        let keys = Object.keys(audios);
+        for (let i = 0; i < keys.length; i++) {
+            let key = keys[i];
+            let data = audios[key].constructor === String ? audios[key] : audios[key].base64;
+            this.playingInstances[key] = 0;
+            this.playingDate[key] = new Date();
+            let arrayBuffer;
+            if (data.startsWith('https://') || data.startsWith('http://')) {
+                arrayBuffer = await this.#loadUrl(data)
+            } else {
+                arrayBuffer = AudioAdvManager.base64ToArrayBuffer(data)
+            }
+            this.absList[key] = await this.audioContext.decodeAudioData(arrayBuffer);
+        }
+        return this;
+    }
+    async #loadUrl(url) {
+        let res = await fetch(url);
+        return await res.arrayBuffer();
+    }
+    #isLoaded(id) {
+        return !!(this.absList && this.absList[id]);
+    }
+    async #waitForLoad(timeout) {
+        let time = new Date();
+        while (true) {
+            if (!this.absList) {
+                let ctime = new Date();
+                if (ctime - time > timeout * 1000) throw new Error('timeout');
+                await new Promise(r => setTimeout(r));
+                continue;
+            }
+            break;
+        }
+    }
+    async grant_permission(timeout) {
+        /*
+            웹에서 소리를 내기 위해서는 먼저 권한을 획득해야 한다.
+            권한 획득의 방법은 사용자의 조작을 통해 본 함수를 실행하는 것이다.
+            그러지 않는다면 타임아웃이 될때까지 권한획득을 못하고있다가 reject될것이다.
+        */
+        if (timeout === undefined) timeout = 30;
+        await this.#waitForLoad(timeout);
+        await new Promise(async (resolve, reject) => {
+            let race = false;
+            let pid = setTimeout(() => {
+                if (!race) {
+                    race = true;
+                    this.interacted = false;
+                    reject(new Error('권한획득 실패'));
+                }
+            }, timeout * 1000);
+            await this.play('__silent');
+            if (!race) {
+                race = true;
+                this.interacted = true;
+                clearTimeout(pid);
+                resolve();
+            }
+        });
+    }
+    #getGain(id, volume, ignoreLayered) {
+        if (!ignoreLayered && this.playingInstances[id] > 1) {
+            if (volume === undefined) volume = 1;
+            volume = volume / (1 + (this.playingInstances[id] * 0.1));
+        }
+        if (volume !== undefined) {
+            if (!this.gains[id]) {
+                this.gains[id] = this.audioContext.createGain();
+                this.gains[id].connect(this.audioContext.destination);
+            }
+            this.gains[id].gain.value = volume; // 재생중에라도 이거 변경하면 볼륨 반영 됨
+            return this.gains[id];
+        } else {
+            return this.audioContext.destination;
+        }
+    }
+    play(id, volume, period, ignoreLayered) {
+        /*
+            grant_permission 에 의해서 권한획득이 성공되지 못하면 실행되지 못한다.
+            id 를 제외한 나머지는 선택사항이다.
+            volume 은 기본으로 1로 셋팅된다.
+            period는 마지막재생으로부터 정해진 시간 이내에는 또 재생할 수 없고 단위는 ms이다.
+            중첩해서 재생을 하면 소리가 커지게 된다. 기본적으로 동시에 재생하더라도 볼륨이 동일하도록 밸런스를 맞춘다. 그런데 이 기능을 무시하고자 한다면 ignoreLayered 를 true로 해주자.
+        */
+        if (volume === undefined) volume = 1;
+        let abort = false;
+        if (!this.interacted) abort = true;
+        if (id === '__silent') abort = false;
+        if (!this.#isLoaded(id)) return;
+        if (abort || !this.absList) return;
+        if (period) {
+            if (new Date() - this.playingDate[id] < period) return;
+            this.playingDate[id] = new Date();
+        }
+        this.playingInstances[id]++;
+        const source = this.audioContext.createBufferSource();
+        source.buffer = this.absList[id];
+        source.loop = false; // true로 하면 무한반복
+        source.connect(this.#getGain(id, volume, ignoreLayered)); // gainNode 안할거면 audioContext.destination
+        source.start();
+        return new Promise(resolve => source.addEventListener('ended', () => {
+            this.playingInstances[id]--;
+            resolve(true);
+        }));
+    }
+    static arrayBufferToBase64(buffer) {
+        var binary = '';
+        var bytes = new Uint8Array(buffer);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+    }
+    static base64ToArrayBuffer(base64) {
+        var binary_string = window.atob(base64);
+        var len = binary_string.length;
+        var bytes = new Uint8Array(len);
+        for (var i = 0; i < len; i++) {
+            bytes[i] = binary_string.charCodeAt(i);
+        }
+        return bytes.buffer;
+    }
+};
